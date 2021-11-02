3767784b2f10850e12c16230dae0f139f6f25f68
karinamina3 <00012217@uca.edu.sv>
2021-11-01 21:37:22 -0600

    changes in router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import AuthProvider from './context/AuthProvider'
-import {Switch, Route} from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import Register from './components/forms/RegisterForm'
 import Login from './components/forms/LoginForm'
 import LayoutDashboard from "./Layout/LayoutDashboard";
@@ -10,8 +10,8 @@ export const App = () => {
   return (
     <AuthProvider>
       <Switch>
-        <Route exact path="/" render={()=><Redirect exact to="/home" />} />
-        <LayoutDashboard path="/home" />
+        <Route exact path="/" render={() => <Redirect exact to="/login" />} />
+        <Route exact path="/home" component={LayoutDashboard} />
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
       </Switch>
