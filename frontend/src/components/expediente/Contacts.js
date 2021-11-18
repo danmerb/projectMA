997dd24c8113d758a06c5541b68aeb0bda0a1908
dannielarenderos <00087717@uca.edu.sv>
2021-11-18 16:28:15 -0600

    Cambios ver expedientes

--- frontend/src/components/expediente/Contacts.js
+++ frontend/src/components/expediente/Contacts.js
@@ -2,11 +2,18 @@ import React, { useContext } from "react";
 import DataContext from "../../context/data-context";
 import { useHistory } from "react-router-dom";
 import { deleteExpediente } from "../../firebase/firebase";
+import { Card, Avatar, Col, Row, List, Image } from 'antd';
+import { EditOutlined, DeleteOutlined, EyeOutlined } from '@ant-design/icons';
+import 'antd/dist/antd.css';
+import '../../app.css';
+import { getImage } from "../../firebase/firebase";
 
+
+
+const { Meta } = Card;
 const Contacts = () => {
   const { expedientes } = useContext(DataContext);
   const history = useHistory();
-
   const expedienteDetail = (paciente) => {
     history.push(`${history.location.pathname}/detail`, paciente);
   };
@@ -15,53 +22,57 @@ const Contacts = () => {
     history.push(`${history.location.pathname}/edit`, paciente);
   };
 
-
   return (
-    <>
-      <table className="table table-stripped">
-        <thead className="thead-light">
-          <tr>
-            <th>Nombre</th>
-            <th>Correo Electrónico</th>
-            <th>Teléfono</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {expedientes.map((paciente) => {
-            return (
-              <tr key={paciente.id}>
-                <td>{paciente.nombre}</td>
-                <td>{paciente.correo}</td>
-                <td>{paciente.telefono}</td>
-                <td>
-                  <p
-                    className="btn text-success"
-                    onClick={() => {
-                      expedienteDetail(paciente);
-                    }}
-                  >
-                    <i className="far fa-eye"></i>
-                  </p>
-                  <p className="btn text-primary" onClick={() => {
-                    expedientEdit(paciente);
-                  }}>
-                    <i className="fas fa-pencil-alt"></i>
-                  </p>
-                  <p className="btn text-danger" onClick={() => {
-                    deleteExpediente(paciente.id);
+    <List
+      grid={{
+        gutter: 16,
+        xs: 1,
+        sm: 2,
+        md: 4,
+        lg: 4,
+        xl: 6,
+        xxl: 3,
+      }}
+      dataSource={expedientes}
 
-                  }}>
-                    <i className="far fa-trash-alt"></i>
-                  </p>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </>
+      renderItem={paciente => (
+      <>
+      
+        < List.Item key={paciente.id} >
+          {console.log(paciente)}
+
+          <Card actions={[
+            <EyeOutlined key="see" onClick={() => {
+              expedienteDetail(paciente);
+            }} />,
+            <EditOutlined key="edit" onClick={() => {
+              expedientEdit(paciente);
+            }} />,
+            <DeleteOutlined key="ellipsis" onClick={() => {
+              deleteExpediente(paciente.id);
+
+            }} />,
+          ]}
+          >
+
+            <Meta
+              avatar={< Avatar src={ paciente.img} />}
+              title={'Nombre: ' + paciente.nombre}
+              description={'Teléfono: ' + paciente.telefono}
+            />
+          </Card>
+        </List.Item>
+        </>
+      )
+      }
+    />
+
+
   );
+
+
+
+
 };
 
 export default Contacts;
