0533b6070561c2229f53cb8d1201bfb0205a7674
dannielarenderos <00087717@uca.edu.sv>
2021-11-23 02:16:05 -0600

    Receta terminado

--- frontend/src/components/receta/ViewReceta.js
+++ frontend/src/components/receta/ViewReceta.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from 'react';
+import React,{useState,useEffect, useContext} from 'react';
 import { Row, Card, Col, Image } from "antd";
 import { MedicineBoxOutlined } from "@ant-design/icons";
 import 'antd/dist/antd.css';
@@ -7,6 +7,7 @@ import '../../style/receta.css';
 import { useHistory } from "react-router";
 import moment from 'moment'
 import { RecaptchaVerifier } from '@firebase/auth';
+import AuthContext from "../../context/auth-context";
 
 const ViewReceta = React.forwardRef((props,ref) => {
     const [receta, setReceta] = useState({});    
@@ -18,36 +19,14 @@ const ViewReceta = React.forwardRef((props,ref) => {
     
     console.log(receta);
     
-    
+    const { currentUser } = useContext(AuthContext);
+
     const gridStyle = {
       width: '100%',
       textAlign: 'center',
       alignItems: 'center'
     };
     
-   /*
-    const data = [
-        {
-            nombrePa: 'Alonso Garcia',
-            edad:"22",
-            genero:"M",            
-            fechaPr: "2020-11-01",
-            idDoc: "mICBP1F7eRdOIlEZcFcszP1FUYB3",
-            nombreCom: "OXYCONTI",
-            nombreGen:"OXYCODONA",
-            presentacion:"Caja de 30 tabletas de 10mg C/U",
-            dosis:"1 tableta cada 6 horas",
-            tiempo: "30 dias "          
-
-        },
-        
-       
-
-
-        
-       
-    ];
-*/ 
     const data = Object.keys(receta).reduce((array, key) => {
         return [...array, {key: receta[key]}]
     }, []);
@@ -94,16 +73,12 @@ const ViewReceta = React.forwardRef((props,ref) => {
     }
 
     
-    
-    //const receta = useState({});
-    //console.log("hola");
-    //console.log(receta);
-    
+  
     return (
         
         <div style={{ justifyContent: "center", alignItems: "center",  textAlign: 'center' }} ref={ref}>
       <Image.PreviewGroup>
-        <Image width={400} src={PictureUp} />
+        <Image width={600} src={PictureUp} />
       </Image.PreviewGroup>
 
       <Row gutter={[16, 16]} style={{ marginTop: "3rem" }} justify="space-between">
@@ -117,8 +92,8 @@ const ViewReceta = React.forwardRef((props,ref) => {
           </Card>
         </Col>
         <Col span={8}>
-          <Card title="Correo Electrónico" bordered={false}>
-            {receta.nombrePa}
+          <Card title="Género" bordered={false}>
+            {receta.genero}
           </Card>
         </Col>
         
@@ -127,83 +102,19 @@ const ViewReceta = React.forwardRef((props,ref) => {
               {  moment(receta.fechaPr).format("DD-MM-YYYY") }
             </Card>
           </Col>
-        
-      
-        
-          
+    
                
         </Row>
-
-     
-
-        <Card style={{ width: '100%' }} title="Historial de Recetas" bordered={false}>
-
-
+        <Card style={{ width: '100%' }} title="Medicamentos" bordered={false}>
           <table className="table table-stripped" style= {{ width: '100%', textAlign: 'center'}}>
-
             <tbody>
               < Mostrar medicamentos={receta.medicamentos} />
-        
-              </tbody>
+            </tbody>
         </table>
-
-
       </Card>
-        
-        </div>
+    </div>
     );
 });
 
 
 export default ViewReceta;
-/*
-{receta.medicamentos.map((medicamento, index) => (
-            <Col  key={index}>
-          <h4 style={ {marginLeft:"4.5cm"}}> Medicamentos</h4>
-          <h5 style={ {marginLeft:"4.5cm"}}> Nombre comercial: {medicamento.nombreCom}</h5>
-          <h5 style={ {marginLeft:"4.5cm"}}> Nombre Generico: {medicamento.nombreGen}</h5>
-          <h5 style={ {marginLeft:"4.5cm"}}> Presentacion: {medicamento.presentacion}</h5>
-          <h5 style={ {marginLeft:"4.5cm"}}> Dosis: {medicamento.dosis}</h5>
-          <h5 style={ {marginLeft:"4.5cm"}}> Tiempo de tratamiento: {medicamento.tiempo}</h5>
-          </Col>
-          
-          ))}
-
-*/
-
-
- /* 
- <List
-
-
-                
-                style={{ margin: 5 }}
-                itemLayout="horizontal"
-                dataSource={data}
-                
-                renderItem={item => (
-                    <List.Item>
-                       
-                        <div className="textArea" >
-                        
-                        <h4 >Paciente : {item.nombrePa}</h4>                       
-                        <h4 >Fecha : {item.fechaPr}</h4>
-                        <h4 >Edad : {item.edad}</h4>
-                        <h4 >Genero : {item.genero}</h4>                                      
-                                              
-                        
-                        <h4 >Nombre del medicamento : {item.nombreCom}</h4>
-                        <h4 >Nombre generico : {item.nombreGen}</h4>
-                        <h4 >Presentacion : {item.presentacion}</h4>
-                        <h4 >Dosis : {item.dosis}</h4>
-                        <h4 >Tiempo de tratamiento : {item.tiempo}</h4>
-                        </div>  
-                    
-                        
-                                                 
-                    </List.Item>
-                )}
-
-                      
-            />
- */
